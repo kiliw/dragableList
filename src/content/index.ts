e9d2a49b05db4fa754b4482836ca9a8d8b743525
kiliw <kilocheat@gmail.com>
2019-07-20 16:55:11 +0200

    WIP. Add react-dnd and build a first working solution from the expample. Use react hooks for the state.

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,23 +1,18 @@
-export const listData = {
-  column: {
-    id: 1,
-    entries: [
-      {
-        title: 'First',
-        text: 'Do this!',
-      },
-      {
-        title: 'Second',
-        text: 'Do that!',
-      },
-      {
-        title: 'Third',
-        text: 'Do this than that!',
-      },
-      {
-        title: 'Fourth',
-        text: 'Do this and that!',
-      },
-    ],
+export const createListData = () => [
+  {
+    id: '1',
+    text: 'Do this!',
   },
-}
+  {
+    id: '2',
+    text: 'Do that!',
+  },
+  {
+    id: '3',
+    text: 'Do this than that!',
+  },
+  {
+    id: '4',
+    text: 'Do this and that!',
+  },
+]
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Wrapper } from './components/Wrapper'
-import { Tasks } from './components/Tasks'
+// import { Wrapper } from './components/Wrapper'
+// import { Tasks } from './components/Tasks'
+import { App } from './components/App'
 
-const App = () => (
-  <Wrapper>
-    <Tasks />
-  </Wrapper>
-)
+// const App = () => (
+//   <Wrapper>
+//     <Tasks />
+//   </Wrapper>
+// )
 
 ReactDOM.render(<App />, document.getElementById('root'))
